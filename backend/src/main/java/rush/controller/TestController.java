fe6506c31ab2cbeb5b152193a8de7aa853bc84b8
khdscor <khds_cor@naver.com>
2021-07-04 21:50:45 +0900

    feat: ResponseEntity 를 활용해서 post, get 해봄

--- backend/src/main/java/rush/controller/TestController.java
+++ backend/src/main/java/rush/controller/TestController.java
@@ -1,7 +1,12 @@
 package rush.controller;
+import java.net.URI;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import rush.dto.TestRequest;
@@ -15,10 +20,18 @@ public class TestController {
 
     final TestService testService;
 
-    @GetMapping("/hello")
-    public TestRequest hello(){
+    @PostMapping("/create")
+    public ResponseEntity<Void> create(@RequestBody TestRequest testRequest){
+        TestRequest test = testService.createTest(testRequest);
 
-        return testService.findTest(testService.createTest());
+        return ResponseEntity.status(201).build();
+    }
+
+
+    @GetMapping("/hello")
+    public ResponseEntity<TestRequest> hello(@RequestParam Long id){
+
+        return ResponseEntity.ok().body(testService.findTest(id));
     }
 
 }
